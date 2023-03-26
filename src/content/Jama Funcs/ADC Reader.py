179f0b28a8df0ab0de9a8e9f2d50fa853c4474df
JCzic <jczic.bos@gmail.com>
2023-03-26 08:06:43 +0200

    Description changed on ADC Reader

--- src/content/Jama Funcs/ADC Reader.py
+++ src/content/Jama Funcs/ADC Reader.py
@@ -8,7 +8,7 @@ dict(
     info = dict(
         name        = 'ADC Reader',
         version     = [1, 0, 0],
-        description = ''' Simple voltage reader on an ADC GPIO.
+        description = ''' Simple level and voltage reader on an analog to digital converter (ADC) GPIO.
                           You can specify the dB applied attenuation and the bits resolution.
                       ''',
         author      = 'JC`zic',
